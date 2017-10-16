19591961cd9336ddf55926350f2b063f954429a5
qingw <qingw@jumei.com>
2017-10-16 17:18:29 +0800

    1. 获取 绝地求生 首页的相关 数据成功。

--- JueDiQiuSheng/JueDiQiuSheng/spiders/JueDiQiuSheng.py
+++ JueDiQiuSheng/JueDiQiuSheng/spiders/JueDiQiuSheng.py
@@ -33,7 +33,8 @@ class JueDiQiuSheng(Spider):
         g_l_h_j = midLSelector.css("div.MidLcon.GLHJ")
         g_l_h_j2 = g_l_h_j.css("div.GLHJ-2")
 
-        rmbb(g_l_h_j2)
+        rmbb(g_l_h_j)
+        getOtherKinds(g_l_h_j2)
 
 
 
@@ -54,10 +55,9 @@ class JueDiQiuSheng(Spider):
         # item['url'] = "http://www.baidu.com"
         #
         # yield item
-
-
 # //todo
 
+# 入门必备
 def rmbb(element):
     GLHJtit = element.css("span.GLHJtit")
 
@@ -72,6 +72,36 @@ def rmbb(element):
     linkElements = imgliklistElements.css("li.lik").css("div.link")
     print("获取 " + name + " link 属性：")
     getElement(linkElements)
+
+# 获取 攻略合集 里面 除过 入门必备 的其他分类
+def getOtherKinds(element):
+    # for (Element element: allGonglue) {
+    #     Elements
+    # sTitle = element.select("span.GLHJtit");
+    # String
+    # name = sTitle.get(0).text();
+    # System.out.println("小标题：\n\t" + name);
+    #
+    # Elements
+    # imgliklistElements = element.select("ul.liklist").select("li.line1");
+    # System.out.println("获取 " + name + " 带图片属性：");
+    # getElement(imgliklistElements);
+    # }
+    for e in element:
+        sTitle = e.css("span.GLHJtit")
+        name = sTitle.select("string(.)").extract()[0]
+        print("小标题：\n\t" + name)
+
+        imgliklistElements = e.css("ul.liklist").css("li.line1")
+        print("获取 " + name + " 带图片属性：")
+        getElement(imgliklistElements)
+
+
+    # Elements
+    # imgliklistElements = element.select("ul.liklist").select("li.line1");
+    # System.out.println("获取 " + name + " 带图片属性：");
+    # getElement(imgliklistElements);
+
 
 
 # 获取相关属性
