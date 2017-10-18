14067e351f3156d49a37ea49cd0dbd63cb4e8722
qingw <qingw@jumei.com>
2017-10-18 14:44:38 +0800

    1. 调整 抓包的相关内容，只保留当前需要的数据。 2. 只抓取对应的分类。

--- JueDiQiuSheng/JueDiQiuSheng/pipelines.py
+++ JueDiQiuSheng/JueDiQiuSheng/pipelines.py
@@ -8,13 +8,13 @@ import Constant as Globle
 import pymysql
 
 
-class JuediqiushengPipeline(object):
+class JDQSCategoryPipeline(object):
     host = Globle.Constant.host
     port = Globle.Constant.port
     user = Globle.Constant.user
     password = Globle.Constant.password
     database_name = Globle.Constant.database_name
-    table_name = "JueDiQiuSheng_jdqscategory"
+    table_name = Globle.Constant.table_name
     charset = Globle.Constant.charset
 
     def __init__(self):
@@ -27,60 +27,23 @@ class JuediqiushengPipeline(object):
             cur = self.conn.cursor()
 
             # id = item['id']
-            jid = item['jid']
-            name = item['name']
-            url = item['url']
-            picUrl = item['picUrl']
             categoryId = item['categoryId']
             categoryName = item['categoryName']
+            categoryUrl = item['categoryUrl']
 
-            sql = "INSERT INTO " + self.table_name + " (jid, name, url, picUrl, categoryId, categoryName)" \
-                                                     " VALUES (%s, %s, %s, %s, %s, %s)"
-            cur.execute(sql, (jid, name, url, picUrl, categoryId, categoryName))
-            cur.close()
-            self.conn.commit()
-
-            return item
-        except:
-            pass
-
-    def close_spider(self):
-        self.conn.close()
-
-
-class JDQSDetailPipeline(object):
-    host = Globle.Constant.host
-    port = Globle.Constant.port
-    user = Globle.Constant.user
-    password = Globle.Constant.password
-    database_name = Globle.Constant.database_name
-    table_name = "JueDiQiuSheng_jdqsdetail"
-    charset = Globle.Constant.charset
-
-    def __init__(self):
-        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.password,
-                                    db=self.database_name, charset=self.charset)
-
-    def process_item(self, item, spider):
-        try:
-            # 给库中插入数据
-            cur = self.conn.cursor()
-
-            # id = item['id']
-            jid = item['jid']
-            artifactName = item['artifactName']
-            artifactAuthor = item['artifactAuthor']
-            content = item['content']
-
-            sql = "INSERT INTO " + self.table_name + " (jid, artifactName, artifactAuthor, content)" \
-                                                     " VALUES (%s, %s, %s, %s)"
-            cur.execute(sql, (jid, artifactName, artifactAuthor, content))
+            sql = "INSERT INTO " + self.table_name + " (" \
+                                                     "categoryId, categoryName, categoryUrl" \
+                                                     ") VALUES (" \
+                                                     "%s, %s, %s" \
+                                                     ")"
+            cur.execute(sql, (categoryId, categoryName, categoryUrl))
 
             cur.close()
             self.conn.commit()
 
             return item
-        except:
+        except Exception:
+            print("JDQSCategoryPipeline process_item 出现异常")
             pass
 
     def close_spider(self):
--- JueDiQiuSheng/JueDiQiuSheng/spiders/JueDiQiuSheng.py
+++ JueDiQiuSheng/JueDiQiuSheng/spiders/JueDiQiuSheng.py
@@ -1,9 +1,5 @@
-from parsel import SelectorList
 from scrapy import Selector
 from scrapy.spiders import Spider
-from selenium import webdriver
-
-from JueDiQiuSheng.items import JuediqiushengItem
 
 
 class JueDiQiuSheng(Spider):
@@ -17,148 +13,26 @@ class JueDiQiuSheng(Spider):
 
     def parse(self, response):
         content = response.body.decode("utf-8")
-        # print(content)
+
+        # 需要抓取的分类
+        question = "问题"
 
         selector = Selector(text=content)
 
         # 获取大分类
         midLSelector = selector.css("div.Mid_L")[0]
 
-        midLtit = midLSelector.css("div.MidLtit")
-        bigTitle = midLtit.css("div.tit.t1::text")[0].extract()
-        print("大标题是：" + bigTitle)
+        titleDiv = midLSelector.css("div.MidLcon_R").css("li.mjtit").css("div.title")
+        titList = titleDiv.css("div.tit::text").extract()
+        hrefA = titleDiv.css("div.more").css("a::attr(href)").extract()
 
-        # // 攻略合集
-        g_l_h_j = midLSelector.css("div.MidLcon.GLHJ")
-        g_l_h_j2 = g_l_h_j.css("div.GLHJ-2")
-
-        infoList = []
-
-        infoList += rmbb(g_l_h_j)
-        infoList += getOtherKinds(g_l_h_j2)
-
-        for e in infoList:
-            name = e["name"]
-            url = e["url"]
-            picUrl = e["picUrl"]
-            category_id = e["category_id"]
-            category_name = e["category_name"]
-
-            print(
-                "\tname: " + name +
-                "\tcategory_id: " + category_id +
-                "\tcategory_name: " + category_name
-            )
-
-            yield insertData2DB(name, url, picUrl, category_id, category_name)
-
-
-# 入门必备
-def rmbb(element):
-    infoList = []
-    GLHJtit = element.css("span.GLHJtit")
-
-    name = GLHJtit.css("span::text").extract()[0]
-    print("小标题是：" + name)
-
-    imgliklistElements = element.css("ul.imgliklist")
-    elements = imgliklistElements.css("li.img")
-    print("获取 " + name + " 带图片属性：")
-    infoList += getElement(elements)
-
-    linkElements = imgliklistElements.css("li.lik").css("div.link")
-    print("获取 " + name + " link 属性：")
-    infoList += getElement(linkElements)
-
-    category_url = GLHJtit.css("a::attr('href')").extract()[0]
-    addCategoryData(infoList, name, category_url)
-
-    return infoList
-
-
-# 添加分类属性
-def addCategoryData(infoList, name, category_url):
-    category_id = getCategoryId(category_url)
-    print("获取 " + name + " category_id：" + category_id)
-    for item in infoList:
-        item["category_id"] = category_id
-        item["category_name"] = name
-
-
-# 获取 攻略合集 里面 除过 入门必备 的其他分类
-def getOtherKinds(element):
-    infoList = []
-
-    for e in element:
-        sTitle = e.css("span.GLHJtit")
-        name = sTitle.xpath("string(.)").extract()[0]
-        print("小标题：\n\t" + name)
-
-        imgliklistElements = e.css("ul.liklist").css("li.line1")
-        print("获取 " + name + " 带图片属性：")
-        elementList = getElement(imgliklistElements)
-
-        try:
-            category_url = sTitle.css("a::attr('href')").extract()[0]
-            addCategoryData(elementList, name, category_url)
-        except:
-            pass
-
-        infoList += elementList
-
-    return infoList
-
-
-# 获取相关属性
-def getElement(element):
-    infoList = []
-
-    for e in element:
-        name = ""
-        url = ""
-        picUrl = ""
-
-        try:
-            name = e.xpath("string(.)").extract()[0].strip()
-        except:
-            pass
-
-        try:
-            url = e.css("a::attr(href)").extract()[0]
-        except:
-            pass
-
-        try:
-            picUrl = e.css("img::attr(src)").extract()[0]
-        except:
-            pass
-
-        print("\t属性是：\n\t\t" +
-              "name:" + name + "\r\n\t\t" +
-              "url:" + url + "\r\n\t\t" +
-              "picUrl:" + picUrl + "\r\n\t\t")
-
-        print("\r")
-
-        d = {'name': name, 'url': url, 'picUrl': picUrl}
-        infoList.append(d)
-
-    return infoList
-
-
-# 根据 Url 获取 Jid
-def getJid(url):
-    try:
-        start = url.rindex("/")
-        end = url.rindex(".")
-        newUrl = url[(start + 1):end]
-        if newUrl.__contains__("-"):
-            newUrl = newUrl[0:newUrl.rindex("-")]
-    except:
-        newUrl = url
-        pass
-
-    return newUrl
+        for i in range(len(titList)):
+            if question == titList[i]:
+                url = hrefA[i]
+                jid = getCategoryId(url)
+                print("对应的链接是：" + url)
+                print("对应的jid 是：" + jid)
+                yield insertData2DB(question, url)
 
 
 # 获取对应的 category_id
@@ -174,15 +48,13 @@ def getCategoryId(url):
 
 
 # 插入数据到数据库中
-def insertData2DB(name, url, pic_url, category_id, category_name):
-    jid = getJid(url)
+def insertData2DB(name, url):
+    jid = getCategoryId(url)
 
-    item = JuediqiushengItem()
-    item['jid'] = jid
-    item['name'] = name
-    item['url'] = url
-    item['picUrl'] = pic_url
-    item['categoryId'] = category_id
-    item['categoryName'] = category_name
+    from JueDiQiuSheng.items import JDQSDCategory
+    item = JDQSDCategory()
+    item['categoryId'] = jid
+    item['categoryName'] = name
+    item['categoryUrl'] = url
 
     return item
