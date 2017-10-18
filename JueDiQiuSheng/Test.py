16354806ccc7ee0e4ee20a139f09e5ae83f796d2
qingw <qingw@jumei.com>
2017-10-18 18:23:51 +0800

    1. 添加测试时间。

--- JueDiQiuSheng/Test.py
+++ JueDiQiuSheng/Test.py
@@ -1,40 +1,67 @@
+import time
+
 url1 = "http://www.gamersky.com/handbook/201706/919363-67.shtml"
 
 
-def getJid(url):
-    start = url.rindex("/")
-    end = url.rindex(".")
-    newUrl = url[(start + 1):end]
-    if newUrl.__contains__("-"):
-        newUrl = newUrl[0:newUrl.rindex("-")]
-    return newUrl
+# print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 
-def getCategoryId(url):
-    url = url[0: url.__len__() - 1]
-    print(url)
+ttt = time.localtime()
 
-    start = url.rindex("_")
-    categoryId = url[(start + 1):url.__len__()]
-    if categoryId.__contains__("-"):
-        categoryId = categoryId[0:categoryId.rindex("-")]
-    return categoryId
+print(ttt)
+t = time.strftime("%Y-%m-%d", ttt)
+
+print(t)
+
+lt = time.mktime(time.strptime(t, "%Y-%m-%d"))
+print(lt)
 
 
+# 2017-09-28
+
+t1 = time.gmtime(lt)
+print(t1)
+
+# a = "Sat Mar 28 22:24:24 2016"
+# print time.mktime(time.strptime(a,"%a %b %d %H:%M:%S %Y"))
+
+
+
+
+# def getJid(url):
+#     start = url.rindex("/")
+#     end = url.rindex(".")
+#     newUrl = url[(start + 1):end]
+#     if newUrl.__contains__("-"):
+#         newUrl = newUrl[0:newUrl.rindex("-")]
+#     return newUrl
 #
-# # print("Jid=" + getJid(url1))
 #
-# l1 = [1,2,3]
-# l2 = [4,5,6]
+# def getCategoryId(url):
+#     url = url[0: url.__len__() - 1]
+#     print(url)
 #
-# l1 += l2
+#     start = url.rindex("_")
+#     categoryId = url[(start + 1):url.__len__()]
+#     if categoryId.__contains__("-"):
+#         categoryId = categoryId[0:categoryId.rindex("-")]
+#     return categoryId
 #
 #
+# #
+# # # print("Jid=" + getJid(url1))
+# #
+# # l1 = [1,2,3]
+# # l2 = [4,5,6]
+# #
+# # l1 += l2
+# #
+# #
+# #
+# # for a in l1:
+# #     print(a)
 #
-# for a in l1:
-#     print(a)
-
-
-categoryUrl = "http://www.gamersky.com/z/playbattlegrounds/862094_64754/"
-
-print(getCategoryId(categoryUrl))
+#
+# categoryUrl = "http://www.gamersky.com/z/playbattlegrounds/862094_64754/"
+#
+# print(getCategoryId(categoryUrl))
