da816d2362e2f8fd2d0d4533c4c2f7a9d5197276
qingw <qingw@jumei.com>
2018-07-31 20:10:23 +0800

    1. 添加约束必须继承父类。

--- SubPro/SubPro/pipelines.py
+++ SubPro/SubPro/pipelines.py
@@ -4,6 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from abc import abstractmethod, ABCMeta
+
 import Constant as Globle
 import pymysql
 from SubPro.items import SubInfoItem, SubMovieDownloadInfoItem, SubMovieLastestInfoItem
@@ -40,12 +42,15 @@ class BaseSubProPipeline(object):
             print(self.getClassName().__name__ + " process_item 出现异常:" + e.__str__())
             return item
 
+    @abstractmethod
     def executeSql(self, item, cur):
         print("初始化 sql 相关数据")
 
+    @abstractmethod
     def getChildItem(self):
         return "getChildItem"
 
+    @abstractmethod
     def getClassName(self):
         return BaseSubProPipeline
 
@@ -67,20 +72,20 @@ class SubInfoPipeline(BaseSubProPipeline):
         return SubInfoPipeline
 
     def executeSql(self, item, cur):
-            id = item['pid']
-            name = item['name']
-            pic = item['pic']
-            url = item['url']
-            update_time = item['update_time']
-            intro = item['intro']
-            capture_pic = item['capture_pic']
+        id = item['pid']
+        name = item['name']
+        pic = item['pic']
+        url = item['url']
+        update_time = item['update_time']
+        intro = item['intro']
+        capture_pic = item['capture_pic']
 
-            sql = "INSERT INTO " + self.table_name + " (" \
-                                                     "id, name, pic, url, update_time, intro, capture_pic" \
-                                                     ") VALUES (" \
-                                                     "%s, %s, %s, %s, %s, %s, %s" \
-                                                     ")"
-            cur.execute(sql, (id, name, pic, url, update_time, intro, capture_pic))
+        sql = "INSERT INTO " + self.table_name + " (" \
+                                                 "id, name, pic, url, update_time, intro, capture_pic" \
+                                                 ") VALUES (" \
+                                                 "%s, %s, %s, %s, %s, %s, %s" \
+                                                 ")"
+        cur.execute(sql, (id, name, pic, url, update_time, intro, capture_pic))
 
 
 class SubMovieDownloadPipeline(BaseSubProPipeline):
@@ -93,20 +98,20 @@ class SubMovieDownloadPipeline(BaseSubProPipeline):
         return SubMovieDownloadPipeline
 
     def executeSql(self, item, cur):
-            pid = item['pid']
-            fj_name = item['fj_name']
-            fj_number = item['fj_number']
+        pid = item['pid']
+        fj_name = item['fj_name']
+        fj_number = item['fj_number']
 
-            id = pid + fj_number
+        id = pid + fj_number
 
-            fj_download_url = item['fj_download_url']
+        fj_download_url = item['fj_download_url']
 
-            sql = "INSERT INTO " + self.table_name + " (" \
-                                                     "id, pid_id, fj_name, fj_download_url" \
-                                                     ") VALUES (" \
-                                                     "%s, %s, %s, %s" \
-                                                     ")"
-            cur.execute(sql, (id, pid, fj_name, fj_download_url))
+        sql = "INSERT INTO " + self.table_name + " (" \
+                                                 "id, pid_id, fj_name, fj_download_url" \
+                                                 ") VALUES (" \
+                                                 "%s, %s, %s, %s" \
+                                                 ")"
+        cur.execute(sql, (id, pid, fj_name, fj_download_url))
 
 
 class SubMovieLastestPipeline(BaseSubProPipeline):
@@ -119,13 +124,13 @@ class SubMovieLastestPipeline(BaseSubProPipeline):
         return SubMovieLastestPipeline
 
     def executeSql(self, item, cur):
-            pid = item['pid']
-            id = pid
-            fj_number = item['fj_number']
+        pid = item['pid']
+        id = pid
+        fj_number = item['fj_number']
 
-            sql = "INSERT INTO " + self.table_name + " (" \
-                                                     "id, pid_id, fj_number" \
-                                                     ") VALUES (" \
-                                                     "%s, %s, %s" \
-                                                     ")"
-            cur.execute(sql, (id, pid, fj_number))
+        sql = "INSERT INTO " + self.table_name + " (" \
+                                                 "id, pid_id, fj_number" \
+                                                 ") VALUES (" \
+                                                 "%s, %s, %s" \
+                                                 ")"
+        cur.execute(sql, (id, pid, fj_number))
