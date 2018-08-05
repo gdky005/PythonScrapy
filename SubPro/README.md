77888453f04eeece7802b0aeb160258af4f62a28
qingw <qingw@jumei.com>
2018-08-05 23:37:18 +0800

    1. 添加一键运行多个爬虫。

---
+++ SubPro/README.md
@@ -0,0 +1,6 @@
+
+
+### 同时运行多个scrapy爬虫的几种方法（自定义scrapy项目命令）
+
+https://www.cnblogs.com/rwxwsblog/p/4578764.html
+
--- SubPro/SubPro/settings.py
+++ SubPro/SubPro/settings.py
@@ -95,3 +95,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
+
+COMMANDS_MODULE = 'SubPro.commands'
