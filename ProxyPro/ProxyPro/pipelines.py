68eb2a0e676a937f58a0439e2efbb94058a9a150
WangQing <wangqing11@100tal.com>
2019-06-20 15:09:24 +0800

    1. 添加代理地址的获取。

---
+++ ProxyPro/ProxyPro/pipelines.py
@@ -0,0 +1,34 @@
+import Constant as Globle
+import pymysql
+
+
+class ProxyProPipeline(object):
+    host = Globle.Constant.host
+    port = Globle.Constant.port
+    user = Globle.Constant.user
+    password = Globle.Constant.password
+    database_name = Globle.Constant.database_name
+    table_name = Globle.Constant.table_name
+    charset = Globle.Constant.charset
+
+    def __init__(self):
+        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.password,
+                                    db=self.database_name, charset=self.charset)
+
+    def process_item(self, item, spider):
+        # 给库中插入数据
+        cur = self.conn.cursor()
+
+        picUrl = item['picUrl']
+        newPage = item['newPage']
+        name = item['name']
+
+        sql = "INSERT INTO " + self.table_name + " (picUrl, newPage, name) VALUES (%s, %s, %s)"
+        cur.execute(sql, (picUrl, newPage, name))
+        cur.close()
+        self.conn.commit()
+
+        return item
+
+    def close_spider(self):
+        self.conn.close()
---
+++ ProxyPro/ProxyPro/spiders/ProxyPro.py
@@ -0,0 +1,32 @@
+from scrapy import Selector
+from scrapy.spiders import Spider
+from selenium import webdriver
+
+
+class ProxyPro(Spider):
+    name = "ProxyPro"
+    start_urls = [
+        "https://www.xicidaili.com/",
+    ]
+
+    def parse(self, response):
+        content = response.body.decode('utf-8')
+
+        print(content)
+
+        content = content
+        # print("爬取的内容如下：" + content)
+
+        selector = Selector(text=content)
+        proxyItem = selector.css("tr.odd")
+
+        for item in proxyItem:
+            try:
+                td = item.css("td").css("td::text").extract()
+                ip = td[0]
+                port = td[1]
+                area = td[2]
+
+                print("用户 ip:" + ip + ", port:" + port + ", area:" + area)
+            except:
+                pass
