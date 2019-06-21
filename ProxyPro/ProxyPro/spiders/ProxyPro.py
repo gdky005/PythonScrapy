d5e2f8148018a595b350f729d9edf9ca818b1c51
WangQing <wangqing11@100tal.com>
2019-06-21 23:34:33 +0800

    1. 添加快代理。

--- ProxyPro/ProxyPro/spiders/ProxyPro.py
+++ ProxyPro/ProxyPro/spiders/ProxyPro.py
@@ -9,9 +9,9 @@ class ProxyPro(Spider):
     name = "ProxyPro"
     start_urls = [
         # "https://www.xicidaili.com/nn/",
-        "http://www.xiladaili.com/gaoni/",
+        # "http://www.xiladaili.com/gaoni/",
         "https://www.kuaidaili.com/free/",
-        "http://www.nimadaili.com/gaoni/",
+        # "http://www.nimadaili.com/gaoni/",
     ]
 
     # def start_requests(self):
@@ -37,6 +37,9 @@ class ProxyPro(Spider):
 
         if ("xicidaili" in url):
             self.getXiCi(list, selector)
+
+        if ("kuaidaili" in url):
+            self.getKuai(list, selector)
 
     def getXiCi(self, list, selector):
         proxyItem = selector.css("tr.odd")
@@ -97,6 +100,70 @@ class ProxyPro(Spider):
         print("当前可用 ip 的个数是：" + list.__len__().__str__())
         listStr = str(list).replace("'", "\"")
         print(listStr)
-        fileObject = open('ipList_xc.txt', 'a+')
+        fileObject = open('ipList_xc.txt', 'w')
+        fileObject.write(listStr + "\n")
+        fileObject.close()
+
+    def getKuai(self, list, selector):
+        proxyItem = selector.css("tbody").css("tr")
+        print("爬取的个数：\n" + proxyItem.__len__().__str__())
+        availableIpCount = 0
+        count = 0
+        for item in proxyItem:
+            count = count + 1
+            try:
+                data = item.css("td::text").extract()
+                td = data
+                ip = td[0]
+                port = td[1]
+                area = td[4]
+                scheme = str(td[3]).lower()
+
+                scheme = scheme.replace("\n", "").strip()
+
+                if scheme is None or scheme == "":
+                    scheme = "http"
+
+                # print("用户 ip:" + ip + ", port:" + port + ", area:" + area + 'type: ' + scheme)
+
+                try:
+                    url = scheme + "://" + ip + ":" + port
+                    obj = {"type": scheme, "url": url}
+
+                    proxies = {scheme: url}
+                    # 暂时去除检测功能
+                    url1 = "http://httpbin.org/get"
+                    res = requests.get(url1, proxies=proxies, timeout=3)
+                    print("\n\n >>>>>>>>>[ 代理地址 结果 ]>>>>>>>>>>>>>>>\n  " +
+                          url + "->" + str(res.status_code) +
+                          "\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+                          )
+
+                    # 可以使用就录入，否则放弃
+                    print("准备针对域名校验：\n")
+
+                    url2 = "https://www.tohomh123.com"
+                    res = requests.get(url2, proxies=proxies, timeout=5)
+
+                    print("\n >>>>>>>>>[ 域名地址 结果 ]>>>>>>>>>>>>>>>\n  " +
+                          url2 + "->" + str(res.status_code) +
+                          "\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n"
+                          )
+
+                    availableIpCount = availableIpCount + 1
+                    print("录入符合规则的 IP 地址： " + str(availableIpCount) + "/" + str(count) + "\n\n\n")
+                    print(url)
+                    list.append(obj)
+                    # print(obj)
+                except requests.exceptions.Timeout as e:
+                    print(str(e))
+                except:
+                    pass
+            except:
+                pass
+        print("当前可用 ip 的个数是：" + list.__len__().__str__())
+        listStr = str(list).replace("'", "\"")
+        print(listStr)
+        fileObject = open('ipList_kuai.txt', 'w')
         fileObject.write(listStr + "\n")
         fileObject.close()
