017bd84d33c358ed63774b1f2e163f6ca53ef35f
WangQing <wangqing11@100tal.com>
2019-06-22 12:02:10 +0800

    1. 测试文件存储。

--- ProxyPro/scFile.py
+++ ProxyPro/scFile.py
@@ -2,12 +2,32 @@ import json
 import time
 
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
 def getFile(url1, header, proxies1):
     res = requests.get(url1, headers=header, proxies=proxies1, verify=False)
     if 200 == res.status_code:
         print(res.content)
+        return 1
+    else:
+        return 0
+
+
+def getFile1(url1, header):
+    res = requests.get(url1, headers=header, verify=False)
+    if 200 == res.status_code:
+        content = res.content.decode("utf-8")
+
+        print(content)
+
+        fileName = url1[url1.rindex("/") + 1:]
+
+        fileObject = open(fileName, 'w+')
+        fileObject.write(content + "\n")
+        fileObject.close()
+
+
         return 1
     else:
         return 0
@@ -22,26 +42,27 @@ for i in range(1, 2):
     # ipIndex = text[random.randint(0, len(text) - 1)]
     ipIndex = text[i % len(text)]
 
-    headers = {'Host': 'tohomh123.com',
+    headers = {
                'Connection': 'keep-alive',
-               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.26 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/517.36',
-               'Referer': 'http://www.tohomh123.com/'}
-
-    time.sleep(1)
+               'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'}
 
     print(i)
     url = "https://www.tohomh123.com/f-1------updatetime--" + str(i) + ".html"
     # url = "http://httpbin.org/get"
     print(url)
 
-    for ip in text:
-        scheme = ip["type"]
-        domain = ip["url"]
+    # 禁用安全请求警告
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    getFile1(url, headers)
 
-        print(domain)
-
-        proxies = {scheme: domain}
-        code = getFile(url, headers, proxies)
-
-        if 1 == code:
-            break
+    # for ip in text:
+    #     scheme = ip["type"]
+    #     domain = ip["url"]
+    #
+    #     print(domain)
+    #
+    #     proxies = {scheme: domain}
+    #     code = getFile(url, headers, proxies)
+    #
+    #     if 1 == code:
+    #         break
