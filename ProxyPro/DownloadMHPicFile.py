cd1248d581bacd4c64db20979a42e4c18244cdd2
WangQing <wangqing11@100tal.com>
2019-06-24 19:51:07 +0800

    1. 使用新的下载文件。

--- ProxyPro/DownloadMHPicFile.py
+++ ProxyPro/DownloadMHPicFile.py
@@ -1,18 +1,33 @@
 import json
+import os
 import time
+from datetime import datetime
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+page = 1
+pageCount = 5
+
+manhuaDetailPath = "/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/manhuaPic/"
+
+# save_dir_name = str((page - 1) * pageCount) + "-" + str(page * pageCount)
+save_dir_name = str((page - 1) * pageCount) + "-" + str(page * pageCount)
+save_dir = manhuaDetailPath + save_dir_name
+
+isExists = os.path.exists(save_dir)
+if not isExists:
+    os.makedirs(save_dir)
+
 
 def getFile1(url1, header):
-    return getFile(url1, header, None)
+    return getFile(url1, header, None, 0)
 
 
-def getFile(url1, header, proxies1, mid):
+def getFile(url1, header, proxies1, id):
     try:
         if proxies1 is None:
-            res = requests.get(url1, headers=header, verify=False)
+            res = requests.get(url1, headers=header, timeout=5, verify=False)
         else:
             res = requests.get(url1, headers=header, timeout=5, proxies=proxies1, verify=False)
 
@@ -23,11 +38,14 @@ def getFile(url1, header, proxies1, mid):
 
             url2 = url1[0: len(url1) - 1]
 
-            fileName = str(mid) + "_" + url1[url2.rindex("/") + 1:]
+            idStr = str(id)
+            id = idStr.zfill(5)
+            fileName = id + "_" + url1[url2.rindex("/") + 1: len(url1) - 1]
 
             print("获取文件内容成功，准备写入到文件中：" + fileName)
 
-            fileObject = open("/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/manhuaPic/" + fileName, 'w+')
+            fileObject = open(save_dir + "/" + fileName, 'w+')
+            # fileObject = open("/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/manhuaDetail/" + fileName, 'w+')
             fileObject.write(content + "\n")
             fileObject.close()
             return 1
@@ -53,7 +71,7 @@ def getJson(ipListPath):
 
 ipListPath1 = "/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/ipList_nima.txt"
 ipListPath2 = "/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/ipList_xici.txt"
-ipListPath3 = "/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/new_avilable_ip.txt"
+ipListPath3 = "/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/new_detail_avilable_ip.txt"
 json1 = getJson(ipListPath1)
 json2 = getJson(ipListPath2)
 json3 = getJson(ipListPath3)
@@ -77,81 +95,132 @@ def getStr(data):
     return str(len(data))
 
 
+def getFormatTime(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    formatTime1 = "%02d:%02d:%02d" % (h, m, s)
+    # print()
+    return formatTime1
+
+
+# 字符类型的时间
+# timeStr = '2013-10-10 23:40:00'
+def getTimeStamp(timeStr):
+    # 转为时间数组
+    timeArray = time.strptime(timeStr, "%Y-%m-%d %H:%M:%S")
+    timeStamp = int(time.mktime(timeArray))  # # 1381419600
+    # print(timeStr + " ---> ")
+    # print(timeStamp)
+    return timeStamp
+
+
 jsonListNew = jsonList
 
-page = 1
-pageCount = 5
+
+def showCurrentTime(msg):
+    now_time = datetime.now()
+    time_str = datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S')
+    print(msg + ": " + time_str)
+    return time_str
+
+
+def getCurrentTime():
+    now_time = datetime.now()
+    time_str = datetime.strftime(now_time, '%Y_%m_%d_%H_%M_%S')
+    return time_str
+
+
+start_time = showCurrentTime("当前任务开始时间")
+
+
+# 网络层
 urlSource = "http://zkteam.cc/ManHua/jsonMHAllData?page=" + str(page) + "&pageCount=" + str(pageCount)
 
 res = requests.get(urlSource)
 resultList = res.json()['result']
 
-i = 0
+showCurrentTime("从服务器获取数据成功，开始处理")
+
+index = 0
 for data in resultList:
     # ipIndex = text[random.randint(0, len(text) - 1)]
     # ipIndex = text[i % len(text)]
 
     headers = {
         'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'}
+        'User-Agent': 'Mozilla/4.0 (Linux; Android 5.1.1; Nexus 6 Build/JRO03D) AppleWebKit/536.19 (KHTML, like Gecko) Chrome/17.0.1025.166  Safari/532.19'}
 
     url = data['mhNewUrl']
-    mid = data['mid']
-    # url = "http://httpbin.org/get"
+    id = data['id']
     print(url)
 
     # 禁用安全请求警告
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    # getFile1(url, headers)
 
+    showCurrentTime("处理任务中")
+    print("当前 index ---> " + str(index) + "/" + str(pageCount) + ", "
+          + str((page - 1) * pageCount + index) + "/" + str(page * pageCount))
     print("可用的 ip 列表是：" + getStr(jsonListNew) + "/" + getStr(jsonList))
 
-    # for ip in jsonList:
-    #     scheme = ip["type"]
-    #     domain = ip["url"]
-    #
-    #     print(domain)
-    #
-    #     proxies = {scheme: domain}
-    #     code = getFile(url, headers, proxies)
-    #
-    #     if 1 == code:
-    #         break
-    #     else:
-    #         jsonListNew.remove(ip)
-
-    ip = jsonList[i % len(jsonList)]
-    i = i + 1
+    ip = jsonList[index % len(jsonListNew)]
+    index = index + 1
     scheme = ip["type"]
     domain = ip["url"]
 
     print(domain)
 
     proxies = {scheme: domain}
-    code = getFile(url, headers, proxies, mid)
+    code = getFile(url, headers, proxies, id)
 
     if 1 == code:
+        showCurrentTime("成功处理一条")
         continue
     else:
         jsonListNew.remove(ip)
 
-        for ip in jsonList:
+        for ip in jsonListNew:
             scheme = ip["type"]
             domain = ip["url"]
 
             print(domain)
 
             proxies = {scheme: domain}
-            code = getFile(url, headers, proxies, mid)
+            code = getFile(url, headers, proxies, id)
 
             if 1 == code:
+                showCurrentTime("成功处理一条")
                 break
             else:
                 jsonListNew.remove(ip)
 
+            showCurrentTime("使用 新 ip 处理成功")
+            print("可用的 ip 列表是：" + getStr(jsonListNew) + "/" + getStr(jsonList))
+    showCurrentTime("处理完一个任务")
+    print("可用的 ip 列表是：" + getStr(jsonListNew) + "/" + getStr(jsonList))
+
+showCurrentTime("处理任务全部完成")
 print("可用的 ip 列表是：" + getStr(jsonListNew) + "/" + getStr(jsonList))
 
-fileName = "new_avilable_ip.txt"
+showCurrentTime("ip 处理成功，存入文件中")
+fileName = "new_detail_avilable_ip_" + getCurrentTime() + ".txt"
 fileObject = open("/Users/WangQing/PycharmProjects/ScrapyPro/ProxyPro/" + fileName, 'w+')
 fileObject.write(str(jsonListNew).replace("'", "\"") + "\n")
 fileObject.close()
+
+end_time = showCurrentTime("整个流程全部处理完成")
+
+
+print("\n\n\n 任务 开始 时间：" + start_time)
+print("\n 任务 结束 时间：" + end_time + "\n\n\n")
+
+diffTime = getTimeStamp(end_time) - getTimeStamp(start_time)
+print("\n总共耗时: " + str(diffTime) + "秒")
+print("\n总共耗时: " + getFormatTime(diffTime))
+
+
+
+
+
+
+
+
