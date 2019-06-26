f05a7ec3e7124ea2962def7634c2befc6226f2fd
WangQing <wangqing11@100tal.com>
2019-06-26 16:26:12 +0800

    1. 添加精选页面的接口。

--- ManHua/ManHua/items.py
+++ ManHua/ManHua/items.py
@@ -12,6 +12,7 @@ from scrapy import Field
 class ManHuaItem(scrapy.Item):
     # define the fields for your item here like:
     mid = Field()
+    mid2 = Field()
     picUrl = Field()
     name = Field()
     newPage = Field()
