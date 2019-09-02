c8c4d0bb29709d8b7dff98b545d6fac55822eafd
WangQing <wangqing11@100tal.com>
2019-09-02 11:17:02 +0800

    1. 抓取短片小说的详情页面。

--- NovelPro/NovelPro/spiders/NovelPro.py
+++ NovelPro/NovelPro/spiders/NovelPro.py
@@ -5,7 +5,8 @@ from scrapy.spiders import Spider
 class NovelPro(Spider):
     name = "NovelPro"
     start_urls = [
-        "https://m.liyuxiang.net/gushi/duanpianxiaoshuo.html",
+        # "https://m.liyuxiang.net/gushi/duanpianxiaoshuo.html",
+        "https://m.liyuxiang.net/gushi/2/12795.html",
     ]
 
     def __init__(self):
@@ -13,6 +14,7 @@ class NovelPro(Spider):
 
     def parse(self, response):
         content = response.body.decode('utf-8')
+        url = response.url
         # print(content)
 
         # print("爬取的内容如下：" + content)
@@ -20,9 +22,23 @@ class NovelPro(Spider):
         selector = Selector(text=content)
 
         # selector.css("dl.cat-list").css("dd")
-        chapterNameExtract = selector.css("div.box-left").css("ul.article-lists").css("li")
+        chapterNameExtract = selector.css("div.wen-box")
 
         for chapterName in chapterNameExtract:
-            name = chapterName.css("a::text").extract()[0]
-            url = chapterName.css("a::attr(href)").extract()[0]
-            print("我需要的名称：" + name + ", url=" + url)
+            name = chapterName.css("h2::text").extract()[0]
+            author = chapterName.css("div.shiwen").css("div::text").extract()[0].strip()
+            author = author[author.index("：") + 1:author.rindex(")")]
+
+            details = chapterName.css("div.shiwen").css("p::text").extract()
+
+            detailStr = "\n"
+            for index, detailItem in enumerate(details):
+                if index >= len(details) - 4:
+                    break
+                detailStr += "    " + detailItem + "\n"
+
+            print("\n名字=" + name
+                  + ", \nauthor=" + author
+                  + ", \nurl=" + url)
+
+            print("\ndetailStr：" + detailStr)
